e7b71b8d38249a8bb1592f060e8f48fa074849c8
Angel Zapata <azapata@basecampcodingacademy.org>
2017-10-16 14:08:55 -0500

    I've added two functions, function gladiator and function attack. I am going to add more functions to make my gladiator game work.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,19 +1,32 @@
-const $ = require('jquery');
-const other = require('../../lib/other');
-
-function hello() {
-    $('body').append('<h1> Good Bye!</h1>');
-    other.foo();
+function randInt(damage_low,damage_high) {
+    return damage_low + Math.floor(Math.random() * (damage_high,damage_low))
 }
 
-function newFunc() {
-    return 2;
-}
-function add(a, b) {
-    return a + b;
+function Gladiator(health, rage, damage_high, damage_low) {
+    this.health = health,
+    this.rage,
+    this.damage_low,
+    this.damage_high
 }
 
-$(hello);
 
-exports.hello = hello;
-exports.add = add;
+this.attack = function attack(this, other) {
+    var number = this.rage
+    var low = this.damage_low
+    var high = this.damage_high
+    if (number < randInt(0, 100)) {
+        var lost = randInt(low, high)
+        other.health -= lost
+        console.log('\nThe opponent lost'+ lost +'health!\n')
+        this.rage += 15
+    }
+    else {
+        var crit_attack = randInt(low, high) * 2
+        other.health -= crit_attack
+        console.log('\nCritical Attack!! The opponent lost' + crit_attack + 'health\n')
+        self.rage = 0
+
+    }
+
+        }
+
