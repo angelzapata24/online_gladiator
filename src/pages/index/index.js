a726bb2b56ce040b3fa9336d6adfd4dcc72bca6e
Angel Zapata <azapata@basecampcodingacademy.org>
2017-10-16 15:23:59 -0500

    modified my functions and as of now they look like they will work so I will now work on my html

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,49 +1,50 @@
-function randInt(damage_low,damage_high) {
-    return damage_low + Math.floor(Math.random() * (damage_high,damage_low))
+function randInt(damage_low, damage_high) {
+    return damage_low + Math.floor(Math.random() * (damage_high - damage_low));
 }
 
 function Gladiator(health, rage, damage_high, damage_low) {
-    this.health = health,
-    this.rage,
-    this.damage_low,
-    this.damage_high
-}
+    this.health = health;
+    this.rage = rage;
+    this.damage_low = damage_low;
+    this.damage_high = damage_high;
 
-
-this.attack = function attack(this, other) {
-    var number = this.rage
-    var low = this.damage_low
-    var high = this.damage_high
-    if (number < randInt(0, 100)) {
-        var lost = randInt(low, high)
-        other.health -= lost
-        console.log('\nThe opponent lost'+ lost +'health!\n')
-        this.rage += 15
-    }
-    else {
-        var crit_attack = randInt(low, high) * 2
-        other.health -= crit_attack
-        console.log('\nCritical Attack!! The opponent lost' + crit_attack + 'health\n')
-        self.rage = 0
-
-    }
-
-}
-
-this.health = function heal(this) {
-    if (self.health >= 100) {
-        console.log('\nCan not heal over 100 health!\n')
-    }
-    else {
-        console.log('\n')
-        if (this.rage <= 10){
-            console.log('\nYou do not have enough Rage to heal.\n')
+    this.attack = function attack(other) {
+        if (this.rage < randInt(0, 100)) {
+            var lost = randInt(this.damage_low, this.damage_high);
+            other.health -= lost;
+            console.log('\nThe opponent lost' + lost + 'health!\n');
+            this.rage += 15;
+        } else {
+            var crit_attack = randInt(this.damage_low, this.damage_high) * 2;
+            other.health -= crit_attack;
+            console.log(
+                '\nCritical Attack!! The opponent lost' +
+                    crit_attack +
+                    'health\n'
+            );
+            this.rage = 0;
         }
-        else {
-            this.rage = self.rage - 10
-            self.health = min(100, this.health + 20)
+    };
+
+    this.health = function heal() {
+        if (this.health >= 100) {
+            console.log('\nCan not heal over 100 health!\n');
+        } else {
+            console.log('\n');
+            if (this.rage <= 10) {
+                console.log('\nYou do not have enough Rage to heal.\n');
+            } else {
+                this.rage = this.rage - 10;
+                this.health = min(100, this.health + 20);
+            }
         }
-    }
+    };
 
+    this.dead = function is_dead() {
+        if (this.health <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
-
